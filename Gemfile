dd21e220491b1530659593199ec47900f4e266cf
maxence snoy <maxence.snoy@gmail.com>
2016-04-07 11:40:40 +0200

    Cloundinary fixed

--- Gemfile
+++ Gemfile
@@ -17,8 +17,9 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
-gem 'cloudinary'
+
 gem 'carrierwave'
+gem 'cloudinary'
 
 group :development, :test do
   gem 'binding_of_caller'
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,7 +1,9 @@
 class Document < ActiveRecord::Base
+
+  mount_uploader :attachment, PhotoUploader # Tells rails to use this uploader for this model.
+
   belongs_to :contract
 
-  mount_uploader :photo, PhotoUploader
 
 end
 
