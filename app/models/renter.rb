6c9e60102841c416e30741abfa6d2cc9c1c1a5c8
Axel Dewitte <adinbrussels@gmail.com>
2016-04-07 10:36:07 +0200

    adjustment errors page.html

--- app/models/renter.rb
+++ app/models/renter.rb
@@ -2,6 +2,7 @@ class Renter < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :contracts
+  
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
